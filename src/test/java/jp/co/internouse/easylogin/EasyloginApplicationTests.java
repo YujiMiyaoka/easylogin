eced8c58188f0e15bcd7d31c0e84cad3948e0676
YujiMiyaoka <youbow5291@gmail.com>
2024-10-08 15:48:58 +0900

    init

---
+++ src/test/java/jp/co/internouse/easylogin/EasyloginApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.internouse.easylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
